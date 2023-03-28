6464d7e69d92ee456cec4c992a61afd1dc3b49cd
Lukas Paquay <lukas.paquay@student.hepl.be>
2023-03-28 12:14:19 +0200

    ✨ Adding a template for the project

---
+++ src/mainWindow.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class mainWindow {
+    private JPanel panel1;
+    private JLabel centerlabel;
+    private JLabel eastlabel;
+    private JLabel westlabel;
+    private JLabel southlabel;
+    private JLabel northlabel;
+}
