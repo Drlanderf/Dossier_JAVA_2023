69ae06502f63f1f4fa5446e7cd8420a1cf320492
landerf <lukas.paquay@outlook.be>
2023-05-09 11:25:39 +0200

    ✨ Adding config.properties

--- src/controler/Controle.java
+++ src/controler/Controle.java
@@ -2,9 +2,15 @@ package controler;
 
 import view.MainGUI;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Controle {
     // propriétés
     private MainGUI mainGUI;
+    private String nomFenetre;
 
 
     public static void main(String[] arts){
@@ -18,11 +24,35 @@ public class Controle {
      */
     public Controle(){
         try{
+            Properties prop = new Properties();
+            prop.load(new FileInputStream("resources/config.properties"));
+            this.nomFenetre = (String)prop.get("APP_NAME");
+            //System.out.println("Le nom de l'application est : "+this.nomFenetre );
+        } catch(FileNotFoundException e){
+            System.out.println("Erreur ! Fichier non trouve... lors de la récupération du nom de la fenetre");
+
+        } catch (IOException e){
+            System.out.println("Erreur d'IO lors de la récupération du nom de la fenetre");
+        }
+
+
+
+        try{
             mainGUI = new MainGUI(this);
             mainGUI.setVisible(true);
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+    //Setters et getters
+
+
+    public String getNomFenetre() {
+        return nomFenetre;
+    }
+
+    public void setNomFenetre(String nomFenetre) {
+        this.nomFenetre = nomFenetre;
     }
 
     /**
